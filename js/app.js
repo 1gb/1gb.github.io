3b8e325cfc060a26eddc8428f7a6b93f5ecb62ca
Eve Worthington <worthington.ee@gmail.com>
2017-10-22 20:39:27 -0700

    console easter egg

--- js/app.js
+++ js/app.js
@@ -23,4 +23,132 @@ $( document ).ready(function() {
           .addClass('glowyorb').addClass('glowyorb' + (i + 1))
       );
     }
+
+    // ---------- Console Fun
+    var styles = [
+        'background: linear-gradient(#3B0425, #BA5E69)',
+        'color: #FFC86F',
+        'padding: 5px',
+        'line-height: 30px',
+        'text-align: center',
+        'font-size: 12px',
+    ].join(';');
+
+    console.log('%c Hello, friend. I have a special message for you! To see it, type: %chi()', styles, 'background: linear-gradient(#3B0425, #BA5E69); padding: 5px 5px 5px 0; color: white; font-weight: bold; font-size: 12px;');
 });
+
+var lines = [
+  '%c                 \n' +
+  '    \\    /\\      \n' +
+  '     )  ( \')     \n' +
+  '    (  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )    /\\      \n' +
+  '   (    ( -)     \n' +
+  '    \\  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\    /\\      \n' +
+  '     )  ( \')     \n' +
+  '    (  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )    /\\      \n' +
+  '   (    ( -)     \n' +
+  '    \\  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\    /\\      \n' +
+  '     )  ( \')     \n' +
+  '    (  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )    /\\      \n' +
+  '   (    ( -)     \n' +
+  '    \\  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\    /\\      \n' +
+  '     )  ( \')     \n' +
+  '    (  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )    /\\      \n' +
+  '   (    ( -)     \n' +
+  '    \\  /  )      \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\   /V\\      \n' +
+  '     ) (\' \')     \n' +
+  '    (  /   )/    \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )   /V\\      \n' +
+  '   (   (\' \')     \n' +
+  '    \\  /   )_    \n' +
+  '     \\(__)|  hi! \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\   /V\\      \n' +
+  '     ) (\' \')     \n' +
+  '    (  /   )/    \n' +
+  '     \\(__)|  hi! \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )   /V\\      \n' +
+  '   (   (\' \')     \n' +
+  '    \\  /   )_    \n' +
+  '     \\(__)|  hi! \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\   /V\\      \n' +
+  '     ) (\' \')     \n' +
+  '    (  /   )/    \n' +
+  '     \\(__)|   ♥  \n' +
+  '                 ',
+  '%c                 \n' +
+  '    )   /V\\      \n' +
+  '   (   (\' \')     \n' +
+  '    \\  /   )_  ♥ \n' +
+  '     \\(__)|      \n' +
+  '                 ',
+  '%c                 \n' +
+  '    \\   /V\\      \n' +
+  '     ) (^ ^) ♥   \n' +
+  '    (  /   )/    \n' +
+  '     \\(__)|      \n' +
+  '                 \n' +
+  '   Thanks for    \n' +
+  '    visiting!    \n' +
+  '                 '
+];
+
+var style = 'color: white; background-color: black; font-size:15px;';
+
+var hi = function() {
+  function start(counter){
+    if (counter === lines.length - 1) {
+      console.clear();
+      console.log(lines[lines.length - 1], style);
+    } else if(counter < lines.length - 1){
+      setTimeout(function(){
+        counter++;
+        console.clear();
+        console.log(lines[counter], style);
+        start(counter);
+      }, 500);
+    }
+  }
+  start(0);
+  return 'Okay!';
+};
